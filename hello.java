824fc5a79f81638427f1016e3e9a6bb145b039fa
Eshan Pandey <spice.eshan@gmail.com>
2023-01-04 13:26:46 +0530

    new commit

--- hello.java
+++ hello.java
@@ -2,5 +2,14 @@ public class hello{
     public static void main(String[] args){
         System.out.println("Hello bro");
         System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+        System.out.println(2*2*2*2);
+
     }
 }
